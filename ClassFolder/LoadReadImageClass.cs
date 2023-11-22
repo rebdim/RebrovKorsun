e3a1172c3253ca0d3de0863ef9f3e423b496601f
Админ <@K228WSR7>
2023-11-22 11:30:20 +0300

    Добавьте файлы проекта.

---
+++ ClassFolder/LoadReadImageClass.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace RebrovKorsun.ClassFolder
+{
+    class LoadReadImageClass
+    {
+        public static BitmapImage BytesToImage(byte[] array)
+        {
+            if (array != null)
+            {
+
+                using (MemoryStream ms = new MemoryStream(array, 0, array.Length))
+                {
+
+                    var image = new BitmapImage();
+
+                    image.BeginInit();
+
+
+                    image.CacheOption = BitmapCacheOption.OnLoad;
+
+                    image.StreamSource = ms;
+
+                    image.EndInit();
+
+                    return image;
+                }
+            }
+            return null;
+        }
+
+
+        public static byte[] ImageToByte(string selectedPhotoName)
+        {
+
+            Bitmap bitmap = new Bitmap(selectedPhotoName);
+            ImageFormat imageFormat = bitmap.RawFormat;
+
+            var imageToConvert = Image.FromFile(selectedPhotoName);
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                imageToConvert.Save(ms, imageFormat);
+
+
+                return ms.ToArray();
+            }
+
+        }
+    }
+}
---
+++ WindowFolder/AuthorizationWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using RebrovKorsun.ClassFolder;
+
+namespace RebrovKorsun.WindowFolder
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationWindow.xaml
+    /// </summary>
+    public partial class AuthorizationWindow : Window
+    {
+        public AuthorizationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void CloseBtn_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void ExitBtn_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            MBClass.ExitMB();
+        }
+
+        private void LogInBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
